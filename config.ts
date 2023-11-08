9a68c27485543b9413720377dcbd9306129dd732
Kyle <kwright4@umbc.edu>
2023-11-08 16:25:25 -0500

    config revert

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-//var connectionString="https://volunteerindexapi.onrender.com"
-var connectionString="http://localhost:3000"
+var connectionString="https://volunteerindexapi.onrender.com"
+//var connectionString="http://localhost:3000"
 
 export default connectionString
